caa7429e90c5eebe34a6109a369b5ef6b30a997c
robhofman <rob.hofman@student.howest.be>
2015-04-14 20:33:13 +0200

    places api

---
+++ Parking/app/src/main/java/be/howest/nmct/parking/KaartFragment.java
@@ -0,0 +1,30 @@
+package be.howest.nmct.parking;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class KaartFragment extends Fragment {
+
+
+    public KaartFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_kaart, container, false);
+    }
+
+
+}
